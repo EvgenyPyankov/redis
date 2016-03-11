39b4b65b16a28bb919514dd1423f3324cb263599
Евгений <Sleepwalker17@mail.ru>
2016-03-12 01:39:33 +0300

    added new methods

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,6 +1,7 @@
 public interface Constants {
-    String GENRES_LIST = "genres";
-    String MOVIES_LIST = "movies";
-    String ACTORS_LIST = "actors";
+    String GENRES = "genres";
+    String MOVIES = "movies";
+    String ACTORS = "actors";
     String ID = "id";
+    String ERR_DELETE = "You can't delete this item because it is already being used somewhere else";
 }
--- src/main/java/DBContorller.java
+++ src/main/java/DBContorller.java
@@ -1,5 +1,7 @@
 import redis.clients.jedis.*;
 
+import java.util.HashMap;
+
 public class DBContorller implements Constants{
     Jedis jedis;
 
@@ -7,27 +9,66 @@ public class DBContorller implements Constants{
         jedis = new Jedis("localhost", 6379);
     }
 
-    public void addMovie(String movie){
-        jedis.sadd(MOVIES_LIST, movie);
+    public void addMovie(HashMap movie){
+        String id = nextVal();
+        jedis.sadd(MOVIES, id);
+        jedis.hmset(MOVIES + ":" + id, movie);
     }
 
-    public void addActor(String actor){
-        jedis.sadd(ACTORS_LIST, actor);
+    public void addActor(HashMap actor){
+        String id = nextVal();
+        jedis.sadd(ACTORS,id);
+        jedis.hmset(ACTORS +":"+id, actor);
     }
 
     public void addGenre(String genre){
-        jedis.sadd(GENRES_LIST,genre);
+        jedis.sadd(GENRES,genre);
     }
 
-    public void addGenreToMovie(long genre, long movie){
-        //jedis.sadd(GENRES_LIST+":"+"movies",movie);
+    public void addGenreToMovie(String genre, String movie){
+        jedis.sadd(GENRES +":"+genre+"movies",movie);
+        jedis.sadd(MOVIES +":"+movie+"genres",genre);
     }
 
-    public void addActorToMovie(){
+    public void addActorToMovie(String actor, String movie){
+        jedis.sadd(getFullName(ACTORS,actor,"movies"),movie);
+        jedis.sadd(getFullName(MOVIES,movie,"actors"),actor);
+    }
+
+    public void deleteGenre(String genre){
+        if (jedis.smembers(getFullName(GENRES,genre,"movies"))==null)
+            jedis.srem(GENRES,genre);
+        else System.out.println(ERR_DELETE);
+    }
+
+    public void deleteActor(String actor){
+        if (jedis.smembers(getFullName(ACTORS,actor,"movies"))==null)
+            jedis.srem(ACTORS,actor);
+        else System.out.println(ERR_DELETE);
+    }
+
+    public void deleteMovie(String movie){
+        jedis.srem(MOVIES,movie);
+        jedis.del(getFullName(MOVIES,movie,"genres"));
+        jedis.del(getFullName(MOVIES,movie,"actors"));
+        jedis.del(getFullName(MOVIES,movie));
+    }
+
+    private String getFullName(String str1, String str2, String str3){
+        return String.format("%s:%s:%s",str1,str2,str3);
 
     }
 
-    public long nextVal(){
-        return jedis.incr(ID);
+    private String getFullName(String str1, String str2){
+        return String.format("%s:%s:%s",str1,str2);
+
     }
+
+    private String nextVal(){
+        return (jedis.incr(ID)).toString();
+    }
+
+
+
+
 }
